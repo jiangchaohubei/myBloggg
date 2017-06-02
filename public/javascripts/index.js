6598958f612cdd598a526290790de78dea823161
jiangchaohubei <jiangchao_hubei@163.com>
2017-06-02 17:53:43 +0800

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,9 @@
 /**
  * Created by PC on 2017/5/27.
  */
+/**
+ * Created by PC on 2017/5/27.
+ */
 $(document).ready(function () {
         $('.navbar-nav li ').each(function (n, v) {
             $(v).bind('click',function(){
@@ -12,8 +15,55 @@ $(document).ready(function () {
             });
     })
 
+    $('.main .box-right .hot-sort .hot-recommend-ul li').each(function (n, v) {
+        $(v).bind('click',function(){
+            $('.main .box-right .hot-sort .hot-recommend-ul li').each(function (n, b) {
+                $(b).removeClass('active');
+            })
+            $(v).addClass('active');
+            var number=$(v).attr('order');
+            $('#active-line').animate({left:number*93+'px',opacity:'0.8'},"slow");
+        })
+    })
+
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//@ sourceURL=index.js
 
 
 
